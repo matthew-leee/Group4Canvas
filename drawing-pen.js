0eb67097ce1b55b9a0cfed9c00f145d2c1ae5c04
matthew-leee <psynergy.lth@gmail.com>
2018-10-21 00:59:17 +0800

    partially fixed pen tool

--- drawing-pen.js
+++ drawing-pen.js
@@ -1,3 +1,6 @@
+let dragCount = [];
+let c;
+
 class DrawingPen extends PaintFunction {
     constructor(contextReal, contextDraft) {
         super();
@@ -6,8 +9,7 @@ class DrawingPen extends PaintFunction {
     }
 
     onMouseDown(coord) {
-        clickCount++;
-        console.log(clickCount);
+        
         this.contextReal.fillStyle = `${fill}`;
         this.contextReal.fillStyle = `${fill}`;
         this.contextDraft.fillStyle = `${fill}`;
@@ -15,46 +17,73 @@ class DrawingPen extends PaintFunction {
         this.origX = coord[0];
         this.origY = coord[1];
         clickArr.push([coord[0], coord[1]])
+        //Draw circle at click point
         this.contextDraft.beginPath();
         this.contextDraft.arc(this.origX, this.origY, 10, 0, 2 * Math.PI)
         this.contextDraft.stroke();
-        if (clickCount > 0) {
-            this.contextDraft.beginPath();
-            this.contextDraft.moveTo(clickArr[0][0], clickArr[0], [1]);
-            for (let i = 0; i < clickCount; i++) {
-                this.contextDraft.lineTo(clickArr[i][0], clickArr[i][1])
-            };
-            this.contextDraft.stroke();
-
-            this.contextReal.beginPath();
-            this.contextReal.moveTo(clickArr[0][0], clickArr[0], [1]);
-            for (let j = 0; j < clickCount; j++) {
-                this.contextReal.lineTo(clickArr[j][0], clickArr[j][1])
-            };
-            this.contextReal.stroke();
-        }
+        clickCount++;
+        console.log(clickCount);
+        // if (clickCount > 0) {
+        //     this.contextDraft.beginPath();
+        //     this.contextDraft.moveTo(clickArr[0][0], clickArr[0][1]);
+        //     for (let i = 0; i < clickCount; i++) {
+        //         this.contextDraft.lineTo(clickArr[i][0], clickArr[i][1]);
+        //         this.contextDraft.stroke();
+        //     };
+        //     this.contextReal.beginPath();
+        //     this.contextReal.moveTo(clickArr[0][0], clickArr[0][1]);
+        //     for (let j = 0; j < clickCount; j++) {
+        //         this.contextReal.lineTo(clickArr[j][0], clickArr[j][1]);
+        //         this.contextReal.stroke();
+        //     };
+        // }
     }
 
     onDragging(coord) {
+        dragCount.push(1);
         if (clickArr.length > 1) {
-            let x = clickArr.length;
+            c = clickArr.length;
             this.contextDraft.beginPath();
-            this.contextDraft.moveTo(clickArr[x - 2][0], clickArr[x - 2][1]);
-            this.contextDraft.quadraticCurveTo(coord[0], coord[1], clickArr[x - 1][0], clickArr[x - 1][1]);
+            this.contextDraft.moveTo(clickArr[c - 2][0], clickArr[c - 2][1]);
+            this.contextDraft.quadraticCurveTo(coord[0], coord[1], clickArr[c - 1][0], clickArr[c - 1][1]);
             this.contextDraft.stroke();
 
             this.contextReal.beginPath();
-            this.contextReal.moveTo(clickArr[x - 2][0], clickArr[x - 2][1]);
-            this.contextReal.quadraticCurveTo(coord[0], coord[1], clickArr[x - 1][0], clickArr[x - 1][1]);
-            this.contextReal.stroke();
+            this.contextReal.moveTo(clickArr[c - 2][0], clickArr[c - 2][1]);
         }
     }
     onMouseMove() { }
-    onMouseUp() { }
+    onMouseUp(coord) {
+        if (clickCount > 1 && dragCount.length == 0) {
+            this.contextDraft.beginPath();
+            this.contextDraft.moveTo(clickArr[0][0], clickArr[0][1]);
+            for (let i = 0; i < clickCount; i++) {
+                this.contextDraft.lineTo(clickArr[i][0], clickArr[i][1]);
+                this.contextDraft.stroke();
+            };
+            this.contextReal.beginPath();
+            this.contextReal.moveTo(clickArr[0][0], clickArr[0][1]);
+            for (let j = 0; j < clickCount; j++) {
+                this.contextReal.lineTo(clickArr[j][0], clickArr[j][1]);
+                this.contextReal.stroke();
+            };
+        } else if (dragCount.length > 0) {
+            c = clickArr.length;
+            console.log(dragCount);
+            console.log([coord[0], coord[1]]);
+            this.contextReal.quadraticCurveTo(coord[0], coord[1], clickArr[c - 1][0], clickArr[c - 1][1]);
+            this.contextReal.stroke();
+            dragCount.pop();
+        }
+    }
     onMouseLeave() { }
     onMouseEnter() { }
     onKeyEnter() {
         this.contextDraft.clearRect(0, 0, canvasDraft.width, canvasDraft.height);
+        while (clickArr.length > 0){
+            clickArr.pop();
+        }
+        clickCount = 0;
     }
 
     // $("#finish").on("click", () =>{
